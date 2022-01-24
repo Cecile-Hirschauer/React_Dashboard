41ff3d9f0776c85e622fbcc532182ec821bafa9e
cecile.hirschauer <cecile.hirschauer@suricats-consulting.com>
2022-01-24 15:05:35 +0100

    Create UI sidebar navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import DashboardEmmployees from "./Pages/DashboardEmployees/DashboardEmployees";
+import DashboardFinance from "./Pages/DashboardFinance/DashboardFinance";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export default function App() {
   return (
-    <>
+    <Router>
+      <Sidebar />
 
-    </>
+      <Switch>
+        <Route path="/" exact component={DashboardFinance} />
+        <Route path="/DashboardEmmployees" exact component={DashboardEmmployees} />
+      </Switch>
+    
+    </Router>
   );
 }
